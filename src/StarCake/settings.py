dad18043756b320f68170944f0cc37bcb5c69817
fahiz <fahizkppullara@gmail.com>
2023-09-30 09:11:06 +0530

    adding all

--- src/StarCake/settings.py
+++ src/StarCake/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 #DEBUG = True
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
-#ALLOWED_HOSTS=['*']
+#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS=['*']
 
 # Application definition
 
