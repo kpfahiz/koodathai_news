2aeee1e9075a7b0e6ce8688727af07318dab40e0
fahiz <fahizkppullara@gmail.com>
2023-11-05 16:31:43 +0530

    social share whatsapp - title content link

--- src/StarCake/settings.py
+++ src/StarCake/settings.py
@@ -15,7 +15,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
-
 # Application definition
 
 INSTALLED_APPS = [
