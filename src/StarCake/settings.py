70a503df97a40f6a48b93efbce507d804bab781b
fahiz <fahizkppullara@gmail.com>
2023-11-05 09:52:28 +0530

    ads secure issues

--- src/StarCake/settings.py
+++ src/StarCake/settings.py
@@ -15,6 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+
 # Application definition
 
 INSTALLED_APPS = [
